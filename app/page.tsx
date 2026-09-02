eecc8dffdccc36b9d4a2e2b22b6bbae9d4622687
vinispeed001-crypto <vinispeed001@gmail.com>
2026-09-02 12:49:12 -0300

    Redirect home page to venue request form

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center">
-      <p className="font-display text-2xl text-gold">ItsMyBday</p>
-    </main>
-  );
+    redirect("/300-sky-bar/pedido");
 }
